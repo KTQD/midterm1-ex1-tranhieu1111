806cc7f5e99c9f54f9ebf71a8255bebf8ebea150
tranhieu1111 <157661558+tranhieu1111@users.noreply.github.com>
2024-03-14 08:27:56 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,37 @@
-public class Book {
+public class Book extends Item {
+    private String author;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String title, float price, String author, int numberOfPages, String genre) {
+        super(title, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
+
