149b533cccf14d57cdc5ff96c2f3c705a78c8508
tranhieu1111 <157661558+tranhieu1111@users.noreply.github.com>
2024-03-14 08:17:09 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
-public class Item {
+public abstract class Product {
+    public String name;
+    private String description;
+    protected String ID;
+    float price;
+
+    public Product(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void showInfo();
 }
+
