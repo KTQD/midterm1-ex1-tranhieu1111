ad818ef73a587202492c99921916fa4b0386c462
tranhieu1111 <157661558+tranhieu1111@users.noreply.github.com>
2024-03-14 08:30:03 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("My Favorite Song", 15.99f, "Artist Name", 240);
         mp3.showInfo();
 
-        Book book;
+        System.out.println();
+        Book book = new Book("Java Programming", 39.99f, "John Doe", 400, "Programming");
         book.showInfo();
-
     }
 }
